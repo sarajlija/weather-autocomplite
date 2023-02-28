6b2f0a70b0ae72b6feb5b3ce90c17ad2e893f888
Salko <64722632+sarajlija@users.noreply.github.com>
2023-02-28 23:43:53 +0100

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const WEATHER_URL = "https://api.openweathermap.org/data/2.5/forecast?q=Sarajevo&cnt=5&appid="
-export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API
+export const REACT_APP_API_KEY = process.env.REACT_APP_WEATHER_API
